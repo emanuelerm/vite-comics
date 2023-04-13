61a38743038440de36a1376b0c570a4fff621a96
emanuelerm <emanueler00@gmail.com>
2023-04-13 16:18:49 +0200

    NavbarComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,22 +1,29 @@
 <template>
   <header>
     <div class="custom-container">
-      <div class="row justify-content-between">
+      <div class="row justify-content-between align-items-center">
         <div class="col-2">
           <img
             :src="getImagePath(`../assets/imgs/${imgData.imgPath}`)"
             :alt="`${imgData.imgAlt}`"
           />
         </div>
-        <div class="col-6"></div>
+        <div class="col-6">
+          <NavbarComponent />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import NavbarComponent from "./NavbarComponent.vue";
+
 export default {
   name: "HeaderComponent",
+  components: {
+    NavbarComponent,
+  },
   data() {
     return {
       imgData: {
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <ul class="d-flex justify-content-between list-unstyled">
+    <li v-for="navItem in navbarEls" class="text-uppercase">
+      <a :href="`${navItem.hrefLink}`">{{ navItem.textLink }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+  data() {
+    return {
+      navbarEls: [
+        {
+          textLink: "characters",
+          hrefLink: "#",
+          active: false,
+        },
+
+        {
+          textLink: "comics",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "movies",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "tv",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "games",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "collectibles",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "videos",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "fans",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "news",
+          hrefLink: "#",
+          active: false,
+        },
+        {
+          textLink: "shop",
+          hrefLink: "#",
+          active: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style class="scss" scoped>
+a {
+  font-size: 1.1rem;
+  text-decoration: none;
+}
+</style>
